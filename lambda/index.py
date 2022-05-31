8bd958462f222e6b3c4b22912054dc7795e93955
subhasis020299 <subhasisdas125@gmail.com>
2022-05-31 11:13:23 +0530

    added lambda code

---
+++ lambda/index.py
@@ -0,0 +1,15 @@
+import requests
+import json
+
+def handler(event,context):
+    url = "https://jsonplaceholder.typicode.com/posts"
+
+    response = requests.get(url)
+    top_ten_posts = response.json()[:10]
+
+    return {
+        "statusCode": response.status_code,
+        "body": json.dumps(top_ten_posts)
+    }
+
+
