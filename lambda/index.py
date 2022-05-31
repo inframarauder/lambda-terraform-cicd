100e715a57cdb73670a985494ab9cf8ac534d813
subhasis020299 <subhasisdas125@gmail.com>
2022-05-31 11:15:21 +0530

    modified lambda code for testing

--- lambda/index.py
+++ lambda/index.py
@@ -1,15 +1,22 @@
-import requests
+# import requests
+# import json
+
+# def handler(event,context):
+#     url = "https://jsonplaceholder.typicode.com/posts"
+
+#     response = requests.get(url)
+#     top_ten_posts = response.json()[:10]
+
+#     return {
+#         "statusCode": response.status_code,
+#         "body": json.dumps(top_ten_posts)
+#     }
+
+
 import json
 
-def handler(event,context):
-    url = "https://jsonplaceholder.typicode.com/posts"
-
-    response = requests.get(url)
-    top_ten_posts = response.json()[:10]
-
+def handler(event, context):
     return {
-        "statusCode": response.status_code,
-        "body": json.dumps(top_ten_posts)
+        "statusCode": 200,
+        "body": json.dumps({"foo": "bar"})
     }
-
-
